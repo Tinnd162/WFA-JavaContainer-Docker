cfd2c89ac8df29d43f479cf129c606d02e1ec8c7
Tinnd162 <tinnd162@gmail.com>
2021-03-21 23:50:30 +0700

    First commit

---
+++ JavaContainer/JavaContainer.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace JavaContainer
+{
+    partial class JavaContainer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCompile = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(29, 91);
+            this.txtCode.Multiline = true;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(502, 318);
+            this.txtCode.TabIndex = 0;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(29, 34);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(502, 31);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(557, 34);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(231, 63);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Ubuntu:18.04 -- Java";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCompile
+            // 
+            this.btnCompile.Location = new System.Drawing.Point(557, 139);
+            this.btnCompile.Name = "btnCompile";
+            this.btnCompile.Size = new System.Drawing.Size(231, 63);
+            this.btnCompile.TabIndex = 3;
+            this.btnCompile.Text = "Compile";
+            this.btnCompile.UseVisualStyleBackColor = true;
+            this.btnCompile.Click += new System.EventHandler(this.btnCompile_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(557, 248);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(231, 63);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // JavaContainer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnCompile);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtCode);
+            this.Name = "JavaContainer";
+            this.Text = "JavaContainer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCompile;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtCode;
+    }
+}
+
---
+++ JavaContainer/JavaContainer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JavaContainer
+{
+    public partial class JavaContainer : Form
+    {
+        public JavaContainer()
+        {
+            InitializeComponent();
+        }
+        public string ExecuteCommandSync(object command)
+        {
+            try
+            {
+                System.Diagnostics.ProcessStartInfo prStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c" + command);
+                prStartInfo.RedirectStandardOutput = true;
+                prStartInfo.UseShellExecute = false;
+                prStartInfo.CreateNoWindow = true;
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = prStartInfo;
+                proc.Start();
+                string res = proc.StandardOutput.ReadToEnd();
+                return res;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+        public void CreateFileCompile()
+        {
+            string PathCompile = @"C:/DockerFile/compile.sh";
+            string ContentCompile = @"cd /data && javac Demo.java && java Demo " + " \"$@\"";
+            File.WriteAllText(PathCompile, ContentCompile);
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            ExecuteCommandSync("docker run -it -d -v C:/DockerFile:/data  --name javacompile ubuntu:18.04");
+        }
+        private void btnCompile_Click(object sender, EventArgs e)
+        {
+
+            string PathDemo = @"C:/DockerFile/Demo.java";
+            File.WriteAllText(PathDemo, txtCode.Text);
+
+            CreateFileCompile();
+
+            string res = ExecuteCommandSync("docker exec javacompile bash /data/compile.sh" + "  " + txtInput.Text);
+            MessageBox.Show(res, "Notification", MessageBoxButtons.OK);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            ExecuteCommandSync("docker stop javacompile");
+            ExecuteCommandSync("docker rm javacompile");
+        }
+    }
+}
